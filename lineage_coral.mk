b1eed39ef11d29cba55e7888427775b5ce273600
ShivamKumarJha <jha.shivam3@gmail.com>
2020-02-10 21:54:31 +0000

    coral-user 10 QQ1C.200205.002 6084391 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- lineage_coral.mk
+++ lineage_coral.mk
@@ -12,12 +12,12 @@ PRODUCT_BRAND := google
 PRODUCT_DEVICE := coral
 PRODUCT_MANUFACTURER := google
 PRODUCT_NAME := lineage_coral
-PRODUCT_MODEL := mainline
+PRODUCT_MODEL := Pixel 4 XL
 
 PRODUCT_GMS_CLIENTID_BASE := android-google
 TARGET_VENDOR := google
 TARGET_VENDOR_PRODUCT_NAME := coral
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="coral-user 10 QQ1B.200105.004 6031802 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="coral-user 10 QQ1B.200205.002 6084387 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/coral/coral:10/QQ1B.200105.004/6031802:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:10/QQ1B.200205.002/6084387:user/release-keys
